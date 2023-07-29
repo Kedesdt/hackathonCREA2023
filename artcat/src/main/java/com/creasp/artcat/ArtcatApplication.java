79a1f0636a00f013e253a4ea251e035d7698f4cc
Univesp-ErickBento <erickbento.univesp@gmail.com>
2023-07-29 11:15:43 -0300

    Projeto Criado

---
+++ artcat/src/main/java/com/creasp/artcat/ArtcatApplication.java
@@ -0,0 +1,13 @@
+package com.creasp.artcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtcatApplication.class, args);
+	}
+
+}
---
+++ artcat/src/test/java/com/creasp/artcat/ArtcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creasp.artcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
